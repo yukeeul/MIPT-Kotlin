bb402c174eeaf7604d525dbc67e7b0a5b568d104
Yuri Ulianov <yuli1108@mail.ru>
2024-04-28 19:17:41 +0300

    turned off debug logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 val kotlin_version: String by project
+val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -25,6 +26,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
 
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
